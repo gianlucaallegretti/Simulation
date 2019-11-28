744d1c41d76853ffe0c2384f393bce6fbe2beaca
gianlucaallegretti <189611@studenti.unimore.it>
2019-11-28 11:31:10 +0100

    creation semaphores and cars

---
+++ graphics.c
@@ -0,0 +1,125 @@
+#include <allegro.h>
+#include <stdio.h>
+// XWIN and YWIN must be the same
+#define XWIN 600
+#define YWIN 600
+
+// To draw graphics and init allegro
+void drawGraphics(){
+    allegro_init();
+    set_color_depth(8);
+    set_gfx_mode(GFX_AUTODETECT_WINDOWED, XWIN, YWIN, 0, 0);
+    clear_to_color(screen, 2);
+    rectfill(screen, XWIN/2 - 100, 0, (XWIN/2)+ 100, YWIN-1, 8);
+    rectfill(screen, 0, (YWIN/2) - 100, XWIN-1, (YWIN/2) + 100, 8);
+}
+
+// To draw road lines
+void drawLines(){
+    // WEST POSITION
+    int i = 0;
+    for (i = 0; i <= 160; i=i+10){
+        rectfill(screen, i, 224, i+5, 226, 15);
+        rectfill(screen, i, 249, i+5, 251, 15);
+        rectfill(screen, i, 274, i+5, 276, 15);
+    }
+
+    // to make it continue when it's near semaphore
+    rectfill(screen, XWIN/2 - 140, 224, XWIN/2 - 100, 226, 15);
+    rectfill(screen, XWIN/2 - 140, 249, XWIN/2 - 100, 251, 15);
+    rectfill(screen, XWIN/2 - 140, 274, XWIN/2 - 100, 276, 15);
+    // Line to separate 2 different way to go (right-left/left-right)
+    rectfill(screen, 0, YWIN/2 -1, XWIN/2 - 100, YWIN/2 + 1, 15);
+    
+    // Other side
+    for(i = 0; i <= 160; i+=10){
+        rectfill(screen, i, 324, i+5, 326, 15);
+        rectfill(screen, i, 349, i+5, 351, 15);
+        rectfill(screen, i, 374, i+5, 376, 15);
+    }
+
+    rectfill(screen, XWIN/2 - 140, 324, XWIN/2 - 100, 326, 15);
+    rectfill(screen, XWIN/2 - 140, 349, XWIN/2 - 100, 351, 15);
+    rectfill(screen, XWIN/2 - 140, 374, XWIN/2 - 100, 376, 15);
+
+
+
+    // EAST POSITION
+    for (i = 440; i <= 600; i=i+10){
+        rectfill(screen, i, 224, i+5, 226, 15);
+        rectfill(screen, i, 249, i+5, 251, 15);
+        rectfill(screen, i, 274, i+5, 276, 15);
+    }
+
+    // to make it continue when it's near semaphore
+    rectfill(screen, 400, 224, 440, 226, 15);
+    rectfill(screen, 400, 249, 440, 251, 15);
+    rectfill(screen, 400, 274, 440, 276, 15);
+    // Line to separate 2 different way to go (right-left/left-right)
+    rectfill(screen, 400, YWIN/2 - 1, XWIN, YWIN/2 +1, 15);
+    
+    // Other side
+    for(i = 440; i <= 600; i+=10){
+        rectfill(screen, i, 324, i+5, 326, 15);
+        rectfill(screen, i, 349, i+5, 351, 15);
+        rectfill(screen, i, 374, i+5, 376, 15);
+    }
+
+    rectfill(screen, 400, 324, 440, 326, 15);
+    rectfill(screen, 400, 349, 440, 351, 15);
+    rectfill(screen, 400, 374, 440, 376, 15);
+
+
+
+    // NORTH POSITION
+    for (i = 0; i <= 160; i=i+10){
+        rectfill(screen, 224, i, 226, i+5, 15);
+        rectfill(screen, 249, i, 251, i+5, 15);
+        rectfill(screen, 274, i, 276, i+5, 15);
+    }
+
+    // to make it continue when it's near semaphore
+    rectfill(screen, 224, YWIN/2 - 140, 226, YWIN/2 - 100, 15);
+    rectfill(screen, 249, YWIN/2 - 140, 251, YWIN/2 - 100, 15);
+    rectfill(screen, 274, YWIN/2 - 140, 276, YWIN/2 - 100, 15);
+    // Line to separate 2 different way to go (right-left/left-right)
+    rectfill(screen, XWIN/2 - 1, 0, XWIN/2 + 1, YWIN/2 - 100, 15);
+    
+    // Other side
+    for(i = 0; i <= 160; i+=10){
+        rectfill(screen, 324, i, 326, i+5, 15);
+        rectfill(screen, 349, i, 351, i+5, 15);
+        rectfill(screen, 374, i, 376, i+5, 15);
+    }
+
+    rectfill(screen, 324, YWIN/2 - 140, 326, YWIN/2 - 100, 15);
+    rectfill(screen, 349, YWIN/2 - 140, 351, YWIN/2 - 100, 15);
+    rectfill(screen, 374, YWIN/2 - 140, 376, YWIN/2 - 100, 15);
+
+
+
+    // SOUTH POSITION
+    for (i = 440; i <= 600; i=i+10){
+        rectfill(screen, 224, i, 226, i+5, 15);
+        rectfill(screen, 249, i, 251, i+5, 15);
+        rectfill(screen, 274, i, 276, i+5, 15);
+    }
+
+    // to make it continue when it's near semaphore
+    rectfill(screen, 224, 400, 226, 440, 15);
+    rectfill(screen, 249, 400, 251, 440, 15);
+    rectfill(screen, 274, 400, 276, 440, 15);
+    // Line to separate 2 different way to go (right-left/left-right)
+    rectfill(screen, XWIN/2 - 1, 400, XWIN/2 + 1, YWIN, 15);
+    
+    // Other side
+    for(i = 440; i <= 600; i+=10){
+        rectfill(screen, 324, i, 326, i+5, 15);
+        rectfill(screen, 349, i, 351, i+5, 15);
+        rectfill(screen, 374, i, 376, i+5, 15);
+    }
+
+    rectfill(screen, 324, 400, 326, 440, 15);
+    rectfill(screen, 349, 400, 351, 440, 15);
+    rectfill(screen, 374, 400, 376, 440, 15);
+}
--- main.c
+++ main.c
@@ -1,17 +1,44 @@
-#include <allegro.h>
+#include "graphics.c"
+#include "cars.c"
+#include "semaphore.c"
 #include <stdio.h>
-#define XWIN 500
-#define YWIN 500
+
+#define green 10
+#define yellow 14
+#define red 12
 
 int main (int argc, char **argv){
-    allegro_init();
-    int k = 0;
-    set_color_depth(8);
-    set_gfx_mode(GFX_AUTODETECT_WINDOWED, XWIN, YWIN, 0, 0);
-    clear_to_color(screen, 2);
-    rectfill(screen, XWIN/3, 0, (XWIN/3)*2, YWIN-1, 8);
-    rectfill(screen, 0, (YWIN/3), XWIN-1, (YWIN/3)*2, 8);
 
+    int k = 0;
+
+    // To draw graphics
+    drawGraphics();    
+
+    // To draw lines in the road
+    drawLines();
+
+    // To draw cars
+    drawCarsEast();
+    drawCarsNorth();
+    drawCarsSouth();
+    drawCarsWest();
+
+
+    int color [3] = {green, yellow, red};
+    for (int i = 0; ; i ++){
+        if (i == 3) {
+            i = 0;
+            }
+        int realcolor = color[i];
+
+        EastSemaphore(realcolor);
+        NorthSemaphore(realcolor);
+        WestSemaphore(realcolor);
+        SouthSemaphore(realcolor);
+        sleep(3);
+    }
+
+    // when ESC is clicked, the app will be closed
     install_keyboard();
     do{
         if (keypressed()){
