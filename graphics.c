731ebce55054ecf1b285e3d866d52a20ff135516
gianlucaallegretti <189611@studenti.unimore.it>
2019-11-29 18:36:41 +0100

    changes in organization of files and code

--- graphics.c
+++ graphics.c
@@ -123,3 +123,35 @@ void drawLines(){
     rectfill(screen, 349, 400, 351, 440, 15);
     rectfill(screen, 374, 400, 376, 440, 15);
 }
+
+// passing the color to east semaphore
+void EastSemaphore(color){
+    rectfill(screen, 400, 205, 410, 220, color);
+    rectfill(screen, 400, 230, 410, 245, color);
+    rectfill(screen, 400, 255, 410, 270, color);
+    rectfill(screen, 400, 280, 410, 295, color);
+}
+
+// passing the color to north semaphore
+void NorthSemaphore(color){
+    rectfill(screen, 205, 190, 220, 200, color);
+    rectfill(screen, 230, 190, 245, 200, color);
+    rectfill(screen, 255, 190, 270, 200, color);
+    rectfill(screen, 280, 190, 295, 200, color);
+}
+
+// passing the color to west semaphore
+void WestSemaphore(color){
+    rectfill(screen, 190, 305, 200, 320, color);
+    rectfill(screen, 190, 330, 200, 345, color);
+    rectfill(screen, 190, 355, 200, 370, color);
+    rectfill(screen, 190, 380, 200, 395, color);
+}
+
+// passing the color to south semaphore
+void SouthSemaphore(color){
+    rectfill(screen, 305, 400, 320, 410, color);
+    rectfill(screen, 330, 400, 345, 410, color);
+    rectfill(screen, 355, 400, 370, 410, color);
+    rectfill(screen, 380, 400, 395, 410, color);
+}
--- main.c
+++ main.c
@@ -1,48 +1,85 @@
-#include "graphics.c"
-#include "cars.c"
-#include "semaphore.c"
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <allegro.h>
+#include "graphics.c"
 
+#define N 5
+// To define the number of each color for semaphores
 #define green 10
 #define yellow 14
 #define red 12
 
-int main (int argc, char **argv){
+#define XWIN 600
+#define YWIN 600
 
-    int k = 0;
+struct cars_t {
+    sem_t mutex, macchina;
+    int xposition, yposition;
+} cars;
 
-    // To draw graphics
-    drawGraphics();    
+void init_cars (struct cars_t *c) {
+    sem_init (&c->mutex, 0, 1);
+    sem_init (&c->macchina, 0, 0);
 
-    // To draw lines in the road
+    c->xposition = 5;
+    c->yposition = 5;
+}
+
+void *macchina (void *arg){
+    int prova = 0;
+}
+
+void *semaforo (void *arg){
+
+    int i = 0;
+    int color_EO[2] = {green, red};
+    int color_NS[2] = {red, green};
+    for (;;){
+        if (i == 2){
+            i = 0;
+        }
+
+        EastSemaphore(color_EO[i]);
+        WestSemaphore(color_EO[i]);
+        NorthSemaphore(color_NS[i]);
+        SouthSemaphore(color_NS[i]);
+        i++;
+        sleep(10);
+    }
+}
+
+int main(){
+
+    drawGraphics();
     drawLines();
 
-    // To draw cars
-    drawCarsEast();
-    drawCarsNorth();
-    drawCarsSouth();
-    drawCarsWest();
+    pthread_t macchinina, semaforino;
+    pthread_attr_t attr;
+    
+    int id = 0;
+    srand(time(NULL));
 
+    pthread_attr_init(&attr);
+    init_cars(&cars);
 
-    int color [3] = {green, yellow, red};
-    for (int i = 0; ; i ++){
-        if (i == 3) {
-            i = 0;
-            }
-        int realcolor = color[i];
-
-        EastSemaphore(realcolor);
-        NorthSemaphore(realcolor);
-        WestSemaphore(realcolor);
-        SouthSemaphore(realcolor);
-        sleep(3);
+    // N thread cars
+    for (int i = 0; i < N; i++){
+        pthread_create (&macchinina, &attr, macchina, (void *)(__intptr_t)id);
+        id++;
     }
 
-    // when ESC is clicked, the app will be closed
+    // 1 thread semaforo
+    pthread_create (&semaforino, &attr, semaforo, NULL);
+    
     install_keyboard();
+    int k = 0;
     do{
         if (keypressed()){
             k = readkey() >> 8;
         }
+
     } while (k != KEY_ESC);
 }
